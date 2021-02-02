8fa83bda436dfcd4ebb084ba406455f281cdeb15
OKhom <okhom173@gmail.com>
2021-02-03 00:27:20 +0200

    Create User.java

---
+++ src/ua/kiev/prog/users/User.java
@@ -0,0 +1,53 @@
+package ua.kiev.prog.users;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+public class User {
+    private final String login;
+    private final String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public int send(String url, String connect) throws IOException {
+        URL obj = new URL(url);
+        HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+
+        try (OutputStream os = conn.getOutputStream()) {
+            String user = "login=" + this.login + "&password=" + this.password + "&connect=" + connect;
+            os.write(user.getBytes(StandardCharsets.UTF_8));
+            return conn.getResponseCode();
+        }
+    }
+
+    public int logout(String url) throws IOException {
+        URL obj = new URL(url);
+        HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
+
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+
+        try (OutputStream os = conn.getOutputStream()) {
+            String logout = "login=" + this.login + "&a=exit";
+            os.write(logout.getBytes(StandardCharsets.UTF_8));
+            return conn.getResponseCode();
+        }
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
