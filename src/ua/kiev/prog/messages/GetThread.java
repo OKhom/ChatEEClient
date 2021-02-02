5d7b85d48da4c96e1fa2628abf5ee516d27d508b
OKhom <okhom173@gmail.com>
2021-02-03 00:27:02 +0200

    Create GetThread.java

---
+++ src/ua/kiev/prog/messages/GetThread.java
@@ -0,0 +1,42 @@
+package ua.kiev.prog.messages;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import ua.kiev.prog.utils.Utils;
+import ua.kiev.prog.chatroom.Chatroom;
+import ua.kiev.prog.users.User;
+
+public class GetThread implements Runnable {
+    private final Gson gson;
+    private int n;
+    private final User user;
+
+    public GetThread(User user) {
+        gson = new GsonBuilder().create();
+        this.user = user;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while ( ! Thread.interrupted()) {
+                String strBuf = Utils.request(Utils.getURL() + "/get?from=" + n);
+                JsonMessages list = gson.fromJson(strBuf, JsonMessages.class);
+                    if (list != null) {
+                        for (Message m : list.getList()) {
+                            if (m.getFrom().equals(user.getLogin()) ||
+                                    m.getTo().equals(user.getLogin()) ||
+                                    m.getTo().equals("all") ||
+                                    Chatroom.getMemberChatroom(m.getTo(), user.getLogin())) {
+                                System.out.println(m);
+                            }
+                            n++;
+                        }
+                    }
+                Thread.sleep(500);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
